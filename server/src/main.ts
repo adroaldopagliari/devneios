894c18456bb0932c45d43116abbedc3336987a38
Adroaldo J. Pagliari <adroaldo@outlook.com>
2020-08-21 22:42:31 -0300

    refactor: edit export members format

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import './shared/infra/typeorm';
+import AppModule from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
