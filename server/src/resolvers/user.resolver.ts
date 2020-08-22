f2596b54aec5e602ba4784e71fa9cc683a6c5e8a
Adroaldo J. Pagliari <adroaldo@outlook.com>
2020-08-22 17:21:18 -0300

    feat: add graphQL integration

---
+++ server/src/resolvers/user.resolver.ts
@@ -0,0 +1,26 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import User from 'src/models/user.entity';
+import UserInput from 'src/input/user.input';
+import RepoService from '../repo.service';
+
+@Resolver()
+class UserResolver {
+  constructor(private readonly repoService: RepoService) {}
+
+  @Query(() => [User])
+  public async getUsers(): Promise<User[]> {
+    return this.repoService.userRepo.find();
+  }
+
+  @Query(() => User, { nullable: true })
+  public async getUser(@Args('id') id: number): Promise<User> {
+    return this.repoService.userRepo.findOne(id);
+  }
+
+  @Mutation(() => User)
+  public async createAuthor(@Args('data') input: UserInput): Promise<User> {
+    const user = this.repoService.userRepo.create({ email: input.email });
+    return this.repoService.userRepo.save(user);
+  }
+}
+export default UserResolver;
