3c1a6d754a0d3c9cd2acfa13392b85ea9f9b61f6
Adroaldo J. Pagliari <adroaldo@outlook.com>
2020-08-23 20:59:28 -0300

    feat: add mutations and query to get notes

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 
-import './styles.css';
+import { Container } from './styles';
+
+interface Note {
+  id: string;
+  content: string;
+  user: {
+    email: string;
+  };
+}
+
+const GET_ALL_MESSAGES = gql`
+  query {
+    getNotes {
+      id
+      content
+      user {
+        email
+      }
+    }
+  }
+`;
 
 export const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  const { loading, data } = useQuery<{ getNotes: Note[] }>(GET_ALL_MESSAGES);
+
+  if (loading) return <p>Loading ...</p>;
+
+  return (
+    <div>
+      {data?.getNotes.map((item) => (
+        <div key={item.id}>{item.content}</div>
+      ))}
+    </div>
+  );
 };
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { gql } from 'apollo-boost';
+import { useMutation } from '@apollo/react-hooks';
 import { History } from 'history';
 import { FaCheck } from 'react-icons/fa';
 
@@ -8,15 +10,51 @@ type Props = {
   history: History;
 };
 
+export const CREATE_OR_LOGIN_USER = gql`
+  mutation($email: String!) {
+    createOrLoginUser(data: { email: $email }) {
+      id
+    }
+  }
+`;
+
 export const Home: React.FC<Props> = ({ history }) => {
   const [input, setInput] = useState<string>('');
+
+  const [createOrLoginUser, { data }] = useMutation(CREATE_OR_LOGIN_USER);
+
+  useEffect(() => {
+    if (data) {
+      const { createOrLoginUser: user } = data;
+      const { id } = user;
+
+      history.push(`/dashboard?id=${id}`);
+    }
+  }, [data, history]);
+
+  async function handleRegister(e: React.MouseEvent): Promise<void> {
+    e.preventDefault();
+
+    if (!input) {
+      alert('Inser a valid e-mail');
+      return;
+    }
+
+    createOrLoginUser({ variables: { email: input } });
+    setInput('');
+  }
 
   return (
     <Container>
       <Content>
         <form>
-          <Input type="text" placeholder="E-mail" />
-          <Button onClick={() => history.push('/dashboard')}>
+          <Input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="E-mail"
+          />
+          <Button onClick={handleRegister}>
             <FaCheck size={36} color="#fff" />
             <span>Login or register</span>
           </Button>
